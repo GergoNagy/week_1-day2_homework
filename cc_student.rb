914efe864effe6446edd3d8d3770f745b7c73e2a
Stephen Beattie <beattie19@hotmail.co.uk>
2017-07-04 00:01:04 +0100

    added a tiny bit of work for the extention but no useful or even correct attempt

--- cc_student.rb
+++ cc_student.rb
@@ -34,37 +34,70 @@
 # ===========================================================
 # Part B
 
-class SportsTeam
+# class SportsTeam
 
-	attr_reader :name
-	attr_accessor :coach, :players, :points
+# 	attr_reader :name
+# 	attr_accessor :coach, :players, :points
 
-	def initialize(name, players, coach)
-		@name = name
-		@players = players
-		@coach = coach
-		@points = 0
-	end
+# 	def initialize(name, players, coach)
+# 		@name = name
+# 		@players = players
+# 		@coach = coach
+# 		@points = 0
+# 	end
 
-	# def get_name
-	# 	return @name
-	# end
+# 	# def get_name
+# 	# 	return @name
+# 	# end
 	
-	# def get_players
-	# 	return @players
+# 	# def get_players
+# 	# 	return @players
+# 	# end
+
+# 	# def get_coach
+# 	# 	return @coach
+# 	# end
+
+# 	# def coach(new_coach)
+# 	# 	@coach = new_coach
+# 	# end
+
+# 	def add_points(num)
+# 		@points += num
+# 	end
+
+# end
+
+# ===========================================================
+# extension
+
+
+
+class Library 
+
+
+	attr_accessor :book_list
+
+	def initialize(array)
+		@book_list = array
+	end 
+
+	# def list_all_info
+	# 	for book in @books
+	# 	return "#{@array[:title]}, borrowed by #{@array[:rental_details][:student_name]} and due on #{@array[:rental_details][:date]}" 
+	# 	end
 	# end
 
-	# def get_coach
-	# 	return @coach
-	# end
 
-	# def coach(new_coach)
-	# 	@coach = new_coach
-	# end
-
-	def add_points(num)
-		@points += num
+	def list_for_book
+		for item in @book_list
+			if item[:title] == "lord_of_the_rings"
+				return "#{@book_list[:title]}, borrowed by #{@book_list[:rental_details][:student_name]} and due on #{@book_list[:rental_details][:date]}" 
+			end
+		end
 	end
-
 end
+
+
+
 
--- cc_student_spec.rb
+++ cc_student_spec.rb
@@ -39,45 +39,95 @@ require_relative('./cc_student.rb')
 # ===========================================================
 # Part B
 
-	class TestSportsTeam < MiniTest::Test 
+	# class TestSportsTeam < MiniTest::Test 
 
-		def test_get_name
-			team = SportsTeam.new("Lions", ["Person1", "Person2", "Person3"],"Stephen")
-			assert_equal("Lions",team.name)
-		end
+	# 	def test_get_name
+	# 		team = SportsTeam.new("Lions", ["Person1", "Person2", "Person3"],"Stephen")
+	# 		assert_equal("Lions",team.name)
+	# 	end
 			
-		def test_get_players
-			team = SportsTeam.new("Lions", ["Person1", "Person2", "Person3"],"Stephen")
-			assert_equal(["Person1", "Person2", "Person3"],team.players)
-		end
+	# 	def test_get_players
+	# 		team = SportsTeam.new("Lions", ["Person1", "Person2", "Person3"],"Stephen")
+	# 		assert_equal(["Person1", "Person2", "Person3"],team.players)
+	# 	end
 			
-		def test_get_coach
-			team = SportsTeam.new("Lions", ["Person1", "Person2", "Person3"],"Stephen")
-			assert_equal("Stephen",team.coach)
-		end
+	# 	def test_get_coach
+	# 		team = SportsTeam.new("Lions", ["Person1", "Person2", "Person3"],"Stephen")
+	# 		assert_equal("Stephen",team.coach)
+	# 	end
 
-		def test_update
-			team = SportsTeam.new("Lions", ["Person1", "Person2", "Person3"],"Stephen")		
-			team.coach ="Rick"
-			assert_equal("Rick", team.coach)
-		end
+	# 	def test_update
+	# 		team = SportsTeam.new("Lions", ["Person1", "Person2", "Person3"],"Stephen")		
+	# 		team.coach ="Rick"
+	# 		assert_equal("Rick", team.coach)
+	# 	end
 
-		def test_new_player
-			team = SportsTeam.new("Lions", ["Person1", "Person2", "Person3"],"Stephen")		
-			team.players.push("Person4")
-			assert_equal("Person4", team.players.last)
-			p team.players
-		end	
+	# 	def test_new_player
+	# 		team = SportsTeam.new("Lions", ["Person1", "Person2", "Person3"],"Stephen")		
+	# 		team.players.push("Person4")
+	# 		assert_equal("Person4", team.players.last)
+	# 		p team.players
+	# 	end	
 
-		def test_player_in_arr?
-			team = SportsTeam.new("Lions", ["Person1", "Person2", "Person3"],"Stephen")		
-			includes = team.players.include?("Person3")
-			assert_equal(true, includes)
-		end
+	# 	def test_player_in_arr?
+	# 		team = SportsTeam.new("Lions", ["Person1", "Person2", "Person3"],"Stephen")		
+	# 		includes = team.players.include?("Person3")
+	# 		assert_equal(true, includes)
+	# 	end
 
-		def test_points
-			team = SportsTeam.new("Lions", ["Person1", "Person2", "Person3"],"Stephen")		
-			points = team.add_points(2)
-			assert_equal(2, points)
-		end
-	end
+	# 	def test_points
+	# 		team = SportsTeam.new("Lions", ["Person1", "Person2", "Person3"],"Stephen")		
+	# 		points = team.add_points(2)
+	# 		assert_equal(2, points)
+	# 	end
+	# end
+	# 
+
+	# ===========================================================
+
+
+class TestLibrary < MiniTest::Test
+
+	def setup
+
+	@books = [
+    { title: "lord_of_the_rings",
+    rental_details: { 
+     student_name: "Jeff", 
+     date: "01/12/16"
+    	}},
+    { title: "the_hobbit",
+    rental_details: { 
+     student_name: "Steve", 
+     date: "01/03/16"
+    	}},
+    	{ title: "harry_potter",
+    rental_details: { 
+     student_name: "Davie", 
+     date: "07/09/16"
+    	}},
+    	{ title: "animal_farm",
+    rental_details: { 
+     student_name: "Peter", 
+     date: "04/09/16"
+    	}}
+  	]
+
+  	end
+
+  	# def test_list_all_details
+  	# 	booklist = Library.new(@books)
+  	# 	p booklist
+  	# 	assert_equal(@books,booklist.list_all_info)
+  	# end
+
+  	def test_list_for_book
+  		books= Library.new(@books)
+  		assert_equal("lord_of_the_rings, borrowed by Jeff and due on 01/12/16",books.list_for_book)
+  	end
+
+  	# def test_rental_details
+  	# 	booklist = Library.new(@books[0])
+  	# end
+
+end
