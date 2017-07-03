c42b11ada707aeb46f08886dc736abd94a145997
Stephen Beattie <beattie19@hotmail.co.uk>
2017-07-03 16:35:21 +0100

    Added gets methods for each property

--- cc_student.rb
+++ cc_student.rb
@@ -1,32 +1,57 @@
-# Create class
-class CodeclanStudent
 
-	def initialize(name, cohort)
-		@name = name
-		@cohort = cohort.to_i
+# class CodeclanStudent
+
+# 	def initialize(name, cohort)
+# 		@name = name
+# 		@cohort = cohort.to_i
 	
+# 	end
+
+# 	def get_name
+# 		return @name
+# 	end
+
+# 	def get_cohort
+# 		return @cohort
+# 	end
+
+# 	def update(name, cohort)	
+# 		@name = name
+# 		@cohort = cohort
+# 		return "#{@name} is in #{@cohort}"
+# 	end
+
+# 	def student_to_talk 
+# 		return "I can talk!"
+# 	end
+
+# 	def fav_lang(lang)
+# 		return "I love #{lang}"
+# 	end
+
+# end
+
+# ===========================================================
+# Part B
+
+class SportsTeam
+	def initialize(name, players, coach)
+		@name = name
+		@players = players
+		@coach = coach
 	end
 
 	def get_name
 		return @name
 	end
-
-	def get_cohort
-		return @cohort
+	
+	def get_players
+		return @players
 	end
 
-	def update(name, cohort)	
-		@name = name
-		@cohort = cohort
-		return "#{@name} is in #{@cohort}"
+	def get_coach
+		return @coach
 	end
 
-	def student_to_talk 
-		return "I can talk!"
-	end
-
-	def fav_lang(lang)
-		return "I love #{lang}"
-	end
 
 end
--- cc_student_spec.rb
+++ cc_student_spec.rb
@@ -1,36 +1,59 @@
 require('minitest/autorun')
 require_relative('./cc_student.rb')
 
-class TestCodeclanStudent < MiniTest::Test
+# class TestCodeclanStudent < MiniTest::Test
 
-	def test_get_name
-		person = CodeclanStudent.new("Gergo", 14)
-		person_2 = CodeclanStudent.new("Alex", 7)
-		assert_equal("Gergo", person.get_name)
-	end
-	def test_get_cohort
-		person = CodeclanStudent.new("Gergo", 14)
-		person_2 = CodeclanStudent.new("Alex", 7)
-		assert_equal(14, person.get_cohort)
-	end
+# 	def test_get_name
+# 		person = CodeclanStudent.new("Gergo", 14)
+# 		person_2 = CodeclanStudent.new("Alex", 7)
+# 		assert_equal("Gergo", person.get_name)
+# 	end
+# 	def test_get_cohort
+# 		person = CodeclanStudent.new("Gergo", 14)
+# 		person_2 = CodeclanStudent.new("Alex", 7)
+# 		assert_equal(14, person.get_cohort)
+# 	end
 
-	def test_update_name_and_cohort
-		person = CodeclanStudent.new("Greg", 3)
-		per_new = person.update("Wojtek", 4)
+# 	def test_update_name_and_cohort
+# 		person = CodeclanStudent.new("Greg", 3)
+# 		per_new = person.update("Wojtek", 4)
 
-		p person
-		assert_equal("Wojtek is in 4", per_new)
-	end
+# 		p person
+# 		assert_equal("Wojtek is in 4", per_new)
+# 	end
 
-	def test_student_to_talk
-		person = CodeclanStudent.new("Greg", 3)
-		talk = person.student_to_talk
-		assert_equal("I can talk!", talk)
-	end
+# 	def test_student_to_talk
+# 		person = CodeclanStudent.new("Greg", 3)
+# 		talk = person.student_to_talk
+# 		assert_equal("I can talk!", talk)
+# 	end
 
-	def test_fav_lang
-		person = CodeclanStudent.new("Greg", 3)
-		fav = person.fav_lang("Ruby")
-		assert_equal("I love Ruby", fav)
+# 	def test_fav_lang
+# 		person = CodeclanStudent.new("Greg", 3)
+# 		fav = person.fav_lang("Ruby")
+# 		assert_equal("I love Ruby", fav)
+# 	end
+# end
+
+
+# ===========================================================
+# Part B
+
+	class TestSportsTeam < MiniTest::Test 
+
+		def test_get_name
+			team = SportsTeam.new("Lions", ["Person1", "Person2", "Person3"],"Stephen")
+			assert_equal("Lions",team.get_name)
+		end
+			
+		def test_get_players
+			team = SportsTeam.new("Lions", ["Person1", "Person2", "Person3"],"Stephen")
+			assert_equal(["Person1", "Person2", "Person3"],team.get_players)
+		end
+			
+		def test_get_coach
+			team = SportsTeam.new("Lions", ["Person1", "Person2", "Person3"],"Stephen")
+			assert_equal("Stephen",team.get_coach)
+		end
+			
 	end
-end
